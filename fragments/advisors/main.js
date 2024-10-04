1b436a08c3050a51f25f9bff758e77445720648f
kjscfaz <kevin.johnston@scfaz.org>
2024-10-04 16:08:32 -0700

    added advisors

---
+++ fragments/advisors/main.js
@@ -0,0 +1,45 @@
+/**
+* Template Name: Eterna
+* Updated: Sep 18 2023 with Bootstrap v5.3.2
+* Template URL: https://bootstrapmade.com/eterna-free-multipurpose-bootstrap-template/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+(function() {
+  "use strict";
+
+  window.document.title = "Template - " + window.document.title;
+
+  fetch('/layout/topbar.html').then(function(response) {
+    return response.text();
+  }).then(function(html) {
+      document.getElementById("topbar").innerHTML = html;
+  }).catch(function(err) {  
+      console.log('Fetch Error', err);  
+  });
+
+  fetch('/layout/header.html').then(function(response) {
+    return response.text();
+  }).then(function(html) {
+      document.getElementById("header").innerHTML = html;
+  }).catch(function(err) {  
+      console.log('Fetch Error', err);  
+  });
+
+  fetch('/fragments/board/main.html').then(function(response) {
+    return response.text();
+  }).then(function(html) {
+      document.getElementById("main").innerHTML = html;
+  }).catch(function(err) {  
+      console.log('Fetch Error', err);  
+  });
+
+  fetch('/layout/footer.html').then(function(response) {
+    return response.text();
+  }).then(function(html) {
+      document.getElementById("footer").innerHTML = html;
+  }).catch(function(err) {  
+      console.log('Fetch Error', err);  
+  });
+
+})()

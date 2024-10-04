c147cdced7ccf4c4399d61a0ddaa8593b83b48b0
kjscfaz <kevin.johnston@scfaz.org>
2024-10-04 11:38:39 -0700

    refactored home.js

---
+++ assets/js/home.js
@@ -0,0 +1,29 @@
+"use strict";
+(function( window, undefined ) {		
+  class Page { 
+    constructor() {
+      this.eventChannel = new BroadcastChannel('eventChannel');
+      this.eventChannel.onmessage = this.handleEvent.bind(this);
+    }
+      
+    handleEvent(event) {
+      if (event.data.name === 'template.initialized')
+        this.setTitle('Home');
+        this.setMainContent('/components/home.html')
+    }
+
+    setTitle(title) {
+      window.document.title = title + " - " + window.document.title;
+    }
+
+    setMainContent(path) {
+      fetch(path).then(function(response) {
+        return response.text();
+      }).then(function(html) {
+          document.getElementById("main").innerHTML = html;
+      }).catch(function(err) {  
+          console.log('Fetch Error', err);  
+      });
+    }
+  }
+})

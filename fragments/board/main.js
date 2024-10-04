aa02307216cbf5a8b34773f0dcfbc17e2970f2e7
kjscfaz <kevin.johnston@scfaz.org>
2024-10-04 15:14:10 -0700

    created templates

---
+++ fragments/board/main.js
@@ -0,0 +1,37 @@
+/**
+* Template Name: Eterna
+* Updated: Sep 18 2023 with Bootstrap v5.3.2
+* Template URL: https://bootstrapmade.com/eterna-free-multipurpose-bootstrap-template/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+(function() {
+  "use strict";
+
+  window.document.title = "Template - " + window.document.title;
+
+  fetch('/layout/topbar.html').then(function(response) {
+    return response.text();
+  }).then(function(html) {
+      document.getElementById("topbar").innerHTML = html;
+  }).catch(function(err) {  
+      console.log('Fetch Error', err);  
+  });
+
+  fetch('/layout/header.html').then(function(response) {
+    return response.text();
+  }).then(function(html) {
+      document.getElementById("header").innerHTML = html;
+  }).catch(function(err) {  
+      console.log('Fetch Error', err);  
+  });
+
+  fetch('/layout/footer.html').then(function(response) {
+    return response.text();
+  }).then(function(html) {
+      document.getElementById("footer").innerHTML = html;
+  }).catch(function(err) {  
+      console.log('Fetch Error', err);  
+  });
+
+})()

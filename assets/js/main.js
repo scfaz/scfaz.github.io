9869f753ff98bc3354c46e3935d7f34cec0aeb19
kjscfaz <kevin.johnston@scfaz.org>
2024-10-04 14:01:19 -0700

    refactors

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,59 +32,35 @@
       console.log('Fetch Error', err);  
   });
 
+  fetch('/components/home.featured.html').then(function(response) {
+    return response.text();
+  }).then(function(html) {
+      document.getElementById("featured").innerHTML = html;
+  }).catch(function(err) {  
+      console.log('Fetch Error', err);  
+  });
+
+  fetch('/components/home.about.html').then(function(response) {
+    return response.text();
+  }).then(function(html) {
+      document.getElementById("about").innerHTML = html;
+  }).catch(function(err) {  
+      console.log('Fetch Error', err);  
+  });
+
+  fetch('/components/home.clients.html').then(function(response) {
+    return response.text();
+  }).then(function(html) {
+      document.getElementById("clients").innerHTML = html;
+  }).catch(function(err) {  
+      console.log('Fetch Error', err);  
+  });
+
   fetch('/components/footer.html').then(function(response) {
     return response.text();
   }).then(function(html) {
       document.getElementById("footer").innerHTML = html;
   }).catch(function(err) {  
       console.log('Fetch Error', err);  
-  });
-
-  /**
-   * Hero carousel indicators
-   */
-  let heroCarouselIndicators = select("#hero-carousel-indicators")
-  let heroCarouselItems = select('#heroCarousel .carousel-item', true)
-
-  heroCarouselItems.forEach((item, index) => {
-    (index === 0) ?
-    heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "' class='active'></li>":
-      heroCarouselIndicators.innerHTML += "<li data-bs-target='#heroCarousel' data-bs-slide-to='" + index + "'></li>"
-  });
-
-  /**
-   * Clients Slider
-   */
-  new Swiper('.clients-slider', {
-    speed: 400,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 2,
-        spaceBetween: 40
-      },
-      480: {
-        slidesPerView: 3,
-        spaceBetween: 60
-      },
-      640: {
-        slidesPerView: 4,
-        spaceBetween: 80
-      },
-      992: {
-        slidesPerView: 6,
-        spaceBetween: 120
-      }
-    }
   });
 })()

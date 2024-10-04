a1b0925225116163e521427f3d42f2ee3a1ddf03
kjscfaz <kevin.johnston@scfaz.org>
2024-10-04 13:24:01 -0700

    refactoring

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,57 +8,6 @@
 (function() {
   "use strict";
 
-  /**
-   * Easy selector helper function
-   */
-  const select = (el, all = false) => {
-    el = el.trim()
-    if (all) {
-      return [...document.querySelectorAll(el)]
-    } else {
-      return document.querySelector(el)
-    }
-  }
-
-  /**
-   * Easy event listener function
-   */
-  const on = (type, el, listener, all = false) => {
-    let selectEl = select(el, all)
-    if (selectEl) {
-      if (all) {
-        selectEl.forEach(e => e.addEventListener(type, listener))
-      } else {
-        selectEl.addEventListener(type, listener)
-      }
-    }
-  }
-
-  /**
-   * Easy on scroll event listener 
-   */
-  const onscroll = (el, listener) => {
-    el.addEventListener('scroll', listener)
-  }
-
-  /**
-   * Scrolls to an element with header offset
-   */
-  const scrollto = (el) => {
-    let header = select('#header')
-    let offset = header.offsetHeight
-
-    if (!header.classList.contains('header-scrolled')) {
-      offset -= 16
-    }
-
-    let elementPos = select(el).offsetTop
-    window.scrollTo({
-      top: elementPos - offset,
-      behavior: 'smooth'
-    })
-  }
-
   fetch('/components/topbar.html').then(function(response) {
     return response.text();
   }).then(function(html) {
@@ -226,6 +175,7 @@
   /**
    * Skills animation
    */
+  /**
   let skilsContent = select('.skills-content');
   if (skilsContent) {
     new Waypoint({
@@ -239,10 +189,12 @@
       }
     })
   }
+     */
 
   /**
    * Porfolio isotope and filter
    */
+  /**
   window.addEventListener('load', () => {
     let portfolioContainer = select('.portfolio-container');
     if (portfolioContainer) {
@@ -267,17 +219,19 @@
     }
 
   });
+ */
 
   /**
    * Initiate portfolio lightbox 
    */
-  const portfolioLightbox = GLightbox({
-    selector: '.portfolio-lightbox'
-  });
+  //const portfolioLightbox = GLightbox({
+ //   selector: '.portfolio-lightbox'
+ // });
 
   /**
    * Portfolio details slider
    */
+  /**
   new Swiper('.portfolio-details-slider', {
     speed: 400,
     loop: true,
@@ -291,6 +245,7 @@
       clickable: true
     }
   });
+   */
 
   /**
    * Initiate Pure Counter 

1e482413145a7ac1e9a8a96ab68be9327a7cc03e
kjscfaz <kevin.johnston@scfaz.org>
2025-07-02 07:33:36 -0700

    Updated providers

---
+++ assets/js/helpers.js
@@ -0,0 +1,53 @@
+"use strict";
+
+/**
+ * Easy selector helper function
+ */
+const select = (el, all = false) => {
+  el = el.trim()
+  if (all) {
+    return [...document.querySelectorAll(el)]
+  } else {
+    return document.querySelector(el)
+  }
+}
+
+/**
+ * Easy event listener function
+ */
+const on = (type, el, listener, all = false) => {
+  let selectEl = select(el, all)
+  if (selectEl) {
+    if (all) {
+      selectEl.forEach(e => e.addEventListener(type, listener))
+    } else {
+      selectEl.addEventListener(type, listener)
+    }
+  }
+}
+
+/**
+ * Easy on scroll event listener 
+ */
+const onscroll = (el, listener) => {
+  el.addEventListener('scroll', listener)
+}
+
+/**
+ * Scrolls to an element with header offset
+ */
+const scrollto = (el) => {
+  let header = select('#header')
+  let offset = header.offsetHeight
+
+  if (!header.classList.contains('header-scrolled')) {
+    offset -= 16
+  }
+
+  let elementPos = select(el).offsetTop
+  window.scrollTo({
+    top: elementPos - offset,
+    behavior: 'smooth'
+  })
+}
+
---
+++ fragments/home/main.js
@@ -0,0 +1,83 @@
+/**
+* Template Name: Eterna
+* Updated: Sep 18 2023 with Bootstrap v5.3.2
+* Template URL: https://bootstrapmade.com/eterna-free-multipurpose-bootstrap-template/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+(function() {
+  "use strict";
+
+  window.document.title = "Home - " + window.document.title;
+
+  fetch('/layout/topbar.html').then(function(response) {
+    return response.text();
+  }).then(function(html) {
+      document.getElementById("topbar").innerHTML = html;
+  }).catch(function(err) {  
+      console.log('Fetch Error', err);  
+  });
+
+  fetch('/layout/header.html').then(function(response) {
+    return response.text();
+  }).then(function(html) {
+      document.getElementById("header").innerHTML = html;
+      document.getElementById("nav-home").classList.add("active");
+  }).catch(function(err) {  
+      console.log('Fetch Error', err);  
+  });
+
+  fetch('/fragments/home/hero.html').then(function(response) {
+    return response.text();
+  }).then(function(html) {
+      document.getElementById("hero").innerHTML = html;
+  }).catch(function(err) {  
+      console.log('Fetch Error', err);  
+  });
+
+  fetch('/fragments/home/featured.html').then(function(response) {
+    return response.text();
+  }).then(function(html) {
+      document.getElementById("featured").innerHTML = html;
+  }).catch(function(err) {  
+      console.log('Fetch Error', err);  
+  });
+
+  fetch('/fragments/home/about.html').then(function(response) {
+    return response.text();
+  }).then(function(html) {
+      document.getElementById("about").innerHTML = html;
+  }).catch(function(err) {  
+      console.log('Fetch Error', err);  
+  });
+
+  fetch('/fragments/home/partners.html').then(function(response) {
+    return response.text();
+  }).then(function(html) {
+    document.getElementById("clients").innerHTML = html;
+  }).then(function() {
+    new BroadcastChannel('eventChannel').postMessage({
+      name: "partners.initialized",
+      message: null
+    });
+  }).catch(function(err) {  
+    console.log('Fetch Error', err);  
+  });
+
+  fetch('/fragments/home/news.html').then(function(response) {
+    return response.text();
+  }).then(function(html) {
+      document.getElementById("news").innerHTML = html;
+  }).catch(function(err) {  
+      console.log('Fetch Error', err);  
+  });
+
+  fetch('/layout/footer.html').then(function(response) {
+    return response.text();
+  }).then(function(html) {
+      document.getElementById("footer").innerHTML = html;
+  }).catch(function(err) {  
+      console.log('Fetch Error', err);  
+  });
+
+})()
